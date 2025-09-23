4a4eddae795872f29a1a62a546bece43569f07be
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-09-23 21:29:32 +0200

    fix: Use valid chain enum for Citrea pools fallback
    - Changed from CHAIN_UNSPECIFIED to Chain.Ethereum as workaround
    - This allows pools to pass validation in usePoolStatsToPoolOptions
    - Pools should now display correctly in search modal

--- packages/uniswap/src/features/pools/hardcodedPools.ts
+++ packages/uniswap/src/features/pools/hardcodedPools.ts
@@ -1,23 +1,25 @@
 import { PoolStats } from '@uniswap/client-explore/dist/uniswap/explore/v1/service_pb'
+import { Chain } from 'uniswap/src/data/graphql/uniswap-data-api/__generated__/types-and-hooks'
 
 // Hardcoded Citrea pools data for SearchModal fallback when API doesn't support Citrea
 // Using partial data since we only need the minimum required fields for display
+// Note: Using Ethereum chain as a workaround since Citrea is not in the Chain enum
 function createCitreaPoolStats(): PoolStats[] {
   return [
     Object.assign(new PoolStats(), {
       id: '0x21180B20134C8913bfA6dc866e43A114c026169e',
-      chain: 'CHAIN_UNSPECIFIED',
+      chain: Chain.Ethereum,
       protocolVersion: 'V3',
       feeTier: 3000,
       token0: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x4370e27F7d91D9341bFf232d7Ee8bdfE3a9933a0',
         symbol: 'WcBTC',
         name: 'Wrapped Citrea BTC',
         decimals: 18,
       },
       token1: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x14ADf6B87096Ef750a956756BA191fc6BE94e473',
         symbol: 'TFC',
         name: 'TaprootFreakCoin',
@@ -28,18 +30,18 @@ function createCitreaPoolStats(): PoolStats[] {
     }),
     Object.assign(new PoolStats(), {
       id: '0xA69De906B9A830Deb64edB97B2eb0848139306d2',
-      chain: 'CHAIN_UNSPECIFIED',
+      chain: Chain.Ethereum,
       protocolVersion: 'V3',
       feeTier: 3000,
       token0: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x4370e27F7d91D9341bFf232d7Ee8bdfE3a9933a0',
         symbol: 'WcBTC',
         name: 'Wrapped Citrea BTC',
         decimals: 18,
       },
       token1: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x2fFC18aC99D367b70dd922771dF8c2074af4aCE0',
         symbol: 'cUSD',
         name: 'Citrus Dollar',
@@ -50,18 +52,18 @@ function createCitreaPoolStats(): PoolStats[] {
     }),
     Object.assign(new PoolStats(), {
       id: '0xD8C7604176475eB8D350bC1EE452dA4442637C09',
-      chain: 'CHAIN_UNSPECIFIED',
+      chain: Chain.Ethereum,
       protocolVersion: 'V3',
       feeTier: 3000,
       token0: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x4370e27F7d91D9341bFf232d7Ee8bdfE3a9933a0',
         symbol: 'WcBTC',
         name: 'Wrapped Citrea BTC',
         decimals: 18,
       },
       token1: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x36c16eaC6B0Ba6c50f494914ff015fCa95B7835F',
         symbol: 'USDC',
         name: 'USDC (Satsuma)',
@@ -72,18 +74,18 @@ function createCitreaPoolStats(): PoolStats[] {
     }),
     Object.assign(new PoolStats(), {
       id: '0x6006797369E2A595D31Df4ab3691044038AAa7FE',
-      chain: 'CHAIN_UNSPECIFIED',
+      chain: Chain.Ethereum,
       protocolVersion: 'V3',
       feeTier: 3000,
       token0: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x4370e27F7d91D9341bFf232d7Ee8bdfE3a9933a0',
         symbol: 'WcBTC',
         name: 'Wrapped Citrea BTC',
         decimals: 18,
       },
       token1: {
-        chain: 'CHAIN_UNSPECIFIED',
+        chain: Chain.Ethereum,
         address: '0x9B28B690550522608890C3C7e63c0b4A7eBab9AA',
         symbol: 'NUSD',
         name: 'Nectra USD',
