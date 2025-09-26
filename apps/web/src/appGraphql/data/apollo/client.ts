715cc892c7dc897ea30afe70f5cce1959169af87
lapatric <42653152+lapatric@users.noreply.github.com>
2025-09-26 18:01:59 +0200

    fix dev/prd environment config

--- apps/web/src/appGraphql/data/apollo/client.ts
+++ apps/web/src/appGraphql/data/apollo/client.ts
@@ -2,22 +2,17 @@ import { ApolloClient, HttpLink, from } from '@apollo/client'
 import { setupSharedApolloCache } from 'uniswap/src/data/cache'
 import { getDatadogApolloLink } from 'utilities/src/logger/datadog/datadogLink'
 
-// TODO: Re-enable once GraphQL endpoint is configured
-let API_URL = process.env.REACT_APP_AWS_API_ENDPOINT
-if (!API_URL) {
-  // throw new Error('AWS API ENDPOINT MISSING FROM ENVIRONMENT')
-  // Use placeholder URL to prevent initialization errors
-  // GraphqlProviders will prevent actual usage when endpoint is not configured
-  API_URL = 'https://api.juiceswap.xyz/v1/graphql'
+let apiUrl = process.env.REACT_APP_AWS_API_ENDPOINT
+if (!apiUrl) {
+  throw new Error('REACT_APP_AWS_API_ENDPOINT is not configured in environment variables')
 }
 
 // Prevent localhost GraphQL endpoints from being used to avoid CSP errors
-if (API_URL.includes('localhost:42069')) {
-  // Localhost GraphQL endpoint detected, using beta gateway instead
-  API_URL = 'https://beta.gateway.uniswap.org/v1/graphql'
+if (apiUrl.includes('localhost:42069')) {
+  apiUrl = 'https://beta.gateway.uniswap.org/v1/graphql'
 }
 
-const httpLink = new HttpLink({ uri: API_URL })
+const httpLink = new HttpLink({ uri: apiUrl })
 const datadogLink = getDatadogApolloLink()
 
 export const apolloClient = new ApolloClient({
