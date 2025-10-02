aed261c7d3d37681ac0730a148f6dd30c87f79f2
Daniel Padrino <danswarrior1@gmail.com>
2025-10-02 10:06:42 -0300

    Silence StatSig

--- packages/uniswap/src/features/gating/StatsigProviderWrapper.tsx
+++ packages/uniswap/src/features/gating/StatsigProviderWrapper.tsx
@@ -1,13 +1,11 @@
 import { ReactNode, useEffect } from 'react'
 import { config } from 'uniswap/src/config'
 import {
-  StatsigOptions,
   StatsigProvider,
   StatsigUser,
   StorageProvider,
   useClientAsyncInit,
 } from 'uniswap/src/features/gating/sdk/statsig'
-import { statsigBaseConfig } from 'uniswap/src/features/gating/statsigBaseConfig'
 import { logger } from 'utilities/src/logger/logger'
 
 type StatsigProviderWrapperProps = {
@@ -20,22 +18,18 @@ type StatsigProviderWrapperProps = {
 
 export function StatsigProviderWrapper({
   children,
-  options,
-  user,
-  storageProvider,
+  options: _options,
+  user: _user,
+  storageProvider: _storageProvider,
   onInit,
 }: StatsigProviderWrapperProps): ReactNode {
   if (!config.statsigApiKey) {
     throw new Error('statsigApiKey is not set')
   }
 
-  const statsigOptions: StatsigOptions = {
-    ...statsigBaseConfig,
-    storageProvider,
-    ...options,
-  }
+  // Statsig options are configured in the base config
 
-  const { client, isLoading: isStatsigLoading } = useClientAsyncInit(config.statsigApiKey, user, statsigOptions)
+  const { client, isLoading: isStatsigLoading } = useClientAsyncInit()
 
   useEffect(() => {
     if (isStatsigLoading) {
