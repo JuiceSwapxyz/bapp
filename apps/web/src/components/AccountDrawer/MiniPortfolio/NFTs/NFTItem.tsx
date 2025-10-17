e560dd67ab62946f9772e4ec7d127f384e522bc0
lapatric <42653152+lapatric@users.noreply.github.com>
2025-10-17 18:48:10 +0200

    add NFT tab to display JuiceSwap NFTs

--- apps/web/src/components/AccountDrawer/MiniPortfolio/NFTs/NFTItem.tsx
+++ apps/web/src/components/AccountDrawer/MiniPortfolio/NFTs/NFTItem.tsx
@@ -1,16 +1,8 @@
 import { SharedEventName } from '@uniswap/analytics-events'
-import { MouseFollowTooltip, TooltipSize } from 'components/Tooltip'
 import { NftCard } from 'nft/components/card'
 import { VerifiedIcon } from 'nft/components/iconExports'
 import { WalletAsset } from 'nft/types'
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { capitalize } from 'tsafe'
 import { Flex, Text } from 'ui/src'
-import { ExternalLink } from 'ui/src/components/icons/ExternalLink'
-import { Chain } from 'uniswap/src/data/graphql/uniswap-data-api/__generated__/types-and-hooks'
-import { useEnabledChains } from 'uniswap/src/features/chains/hooks/useEnabledChains'
-import { GqlChainId } from 'uniswap/src/features/chains/types'
 import { ElementName } from 'uniswap/src/features/telemetry/constants'
 import { sendAnalyticsEvent } from 'uniswap/src/features/telemetry/send'
 import { useTrace } from 'utilities/src/telemetry/trace/TraceContext'
@@ -24,18 +16,12 @@ export function NFT({
   mediaShouldBePlaying: boolean
   setCurrentTokenPlayingMedia: (tokenId: string | undefined) => void
 }) {
-  const { t } = useTranslation()
-  const { isTestnetModeEnabled, gqlChains } = useEnabledChains()
   const trace = useTrace()
-  const [isHovered, setIsHovered] = useState(false)
-
-  const enabled =
-    asset.chain && isTestnetModeEnabled ? gqlChains.includes(asset.chain as GqlChainId) : asset.chain === Chain.Ethereum
 
   const onPress = () => {
-    if (enabled && asset.asset_contract.address && asset.tokenId) {
+    if (asset.asset_contract.address && asset.tokenId) {
       window.open(
-        `https://opensea.io/item/${asset.chain?.toLowerCase()}/${asset.asset_contract.address}/${asset.tokenId}`,
+        `https://explorer.testnet.citrea.xyz/token/${asset.asset_contract.address}/instance/${asset.tokenId}`,
         '_blank',
         'noopener,noreferrer',
       )
@@ -43,53 +29,33 @@ export function NFT({
   }
 
   return (
-    <Flex
-      gap="8px"
-      minHeight="150px"
-      alignItems="center"
-      justifyContent="flex-start"
-      width="100%"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <MouseFollowTooltip
-        placement="bottom"
-        size={TooltipSize.Max}
-        disabled={enabled}
-        text={t('nft.chainSupportComingSoon', {
-          chainName: capitalize(asset.chain?.toLowerCase() ?? 'L2'),
-        })}
-        hideArrow
-      >
-        <NftCard
-          asset={asset}
-          hideDetails
-          display={{ disabledInfo: true }}
-          isSelected={false}
-          isDisabled={!enabled}
-          onCardClick={onPress}
-          sendAnalyticsEvent={() =>
-            sendAnalyticsEvent(SharedEventName.ELEMENT_CLICKED, {
-              element: ElementName.MiniPortfolioNftItem,
-              collection_name: asset.collection?.name,
-              collection_address: asset.collection?.address,
-              token_id: asset.tokenId,
-              ...trace,
-            })
-          }
-          mediaShouldBePlaying={mediaShouldBePlaying}
-          setCurrentTokenPlayingMedia={setCurrentTokenPlayingMedia}
-          testId="mini-portfolio-nft"
-        />
-      </MouseFollowTooltip>
-      <NFTDetails asset={asset} isHovered={isHovered} />
+    <Flex gap="8px" minHeight="150px" alignItems="center" justifyContent="flex-start" width="100%">
+      <NftCard
+        asset={asset}
+        hideDetails
+        display={{ disabledInfo: true }}
+        isSelected={false}
+        isDisabled={false}
+        onCardClick={onPress}
+        sendAnalyticsEvent={() =>
+          sendAnalyticsEvent(SharedEventName.ELEMENT_CLICKED, {
+            element: ElementName.MiniPortfolioNftItem,
+            collection_name: asset.collection?.name,
+            collection_address: asset.collection?.address,
+            token_id: asset.tokenId,
+            ...trace,
+          })
+        }
+        mediaShouldBePlaying={mediaShouldBePlaying}
+        setCurrentTokenPlayingMedia={setCurrentTokenPlayingMedia}
+        testId="mini-portfolio-nft"
+      />
+      <NFTDetails asset={asset} />
     </Flex>
   )
 }
 
-function NFTDetails({ asset, isHovered }: { asset: WalletAsset; isHovered: boolean }) {
-  const { t } = useTranslation()
-
+function NFTDetails({ asset }: { asset: WalletAsset }) {
   return (
     <Flex overflow="hidden" width="100%" flexWrap="nowrap">
       <Flex row alignItems="center" gap="4px" width="100%">
@@ -100,21 +66,14 @@ function NFTDetails({ asset, isHovered }: { asset: WalletAsset; isHovered: boole
           $platform-web={{ whiteSpace: 'pre', textOverflow: 'ellipsis', overflow: 'hidden' }}
         >
           {asset.asset_contract.name}
+          {asset.tokenId && (
+            <Text variant="body3" color="$neutral3" $platform-web={{ display: 'inline' }}>
+              {' '}
+              #{asset.tokenId}
+            </Text>
+          )}
         </Text>
         {asset.collectionIsVerified && <Verified />}
-      </Flex>
-      <Flex
-        opacity={isHovered ? 1 : 0}
-        row
-        alignItems="center"
-        justifyContent="flex-start"
-        width="100%"
-        gap="$spacing4"
-      >
-        <Text color="$neutral2" variant="body4">
-          {t('common.opensea.link')}
-        </Text>
-        <ExternalLink color="$neutral2" size="$icon.12" />
       </Flex>
     </Flex>
   )
